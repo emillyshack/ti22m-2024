fd30427e28d3bf4c9a848b25a57deb0db69b8bae
emilyshack <163414103+emilyshack@users.noreply.github.com>
2024-08-22 08:54:19 -0300

    aulas

---
+++ 2024-08-22/script.js
@@ -0,0 +1,8 @@
+// Entrada
+let peso = Number(prompt("Informe seu peso"))
+let altura = Number(prompt("Informe sua altura"))
+// Processamento
+let imc = peso/(altura*altura)
+// saída
+document.write("IMC: ", imc)
+
